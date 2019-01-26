a7d6cf0b79e4a273f52580554e913a80b2242032
snc16f <snc16f@my.fsu.edu>
2019-01-26 17:15:19 -0500

    Added 3 more weapon instances -knife,hammer,and random sword

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -28,5 +28,17 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
         return new CommonSpear();
     }
 
+    if (name.compare("knife") == 0) {
+        return new SharpKnife();
+    }
+
+    if (name.compare("hammer") == 0) {
+        return new SimpleHammer();
+    }
+
+    if (name.compare("random sword") == 0) {
+        return new CrazyRandomSword();
+    }
+
     throw "Invalid weapon";
 }
