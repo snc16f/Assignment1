d1c01718e59e9d13d1cb375017166579c5000868
snc16f <snc16f@my.fsu.edu>
2019-02-01 11:55:45 -0500

    Updataed WeaponFactory.cpp to include the 3 new weapon classes as well as #included their .h files

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "CrazyRandomSword.h"
+#include "SharpKnife.h"
+#include "SimpleHammer.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
